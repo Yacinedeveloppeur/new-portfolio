11a6fd44057ad9928d67d904239fe2752606b464
Yacine Developpeur <yacine.developpeur@gmail.com>
2022-01-26 22:23:33 +0100

    Add contact form with netflify

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,46 +5,58 @@ import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="navigation">
-      <ul className="navigation__list">
-        <li className="navigation__list__item navigation__list__item--active">
-          <NavLink
-            className={({ isActive }) =>
-              isActive
-                ? "navigation__list__item__link--active"
-                : "navigation__list__item__link"
-            }
-            exact
-            to="/"
-          >
-            Accueil
-          </NavLink>
-        </li>
-        <li className="navigation__list__item">
-          <NavLink
-            className={({ isActive }) =>
-              isActive
-                ? "navigation__list__item__link--active"
-                : "navigation__list__item__link"
-            }
-            to="/realisations"
-          >
-            Réalisations
-          </NavLink>
-        </li>
-        <li className="navigation__list__item">
-          <NavLink
-            className={({ isActive }) =>
-              isActive
-                ? "navigation__list__item__link--active"
-                : "navigation__list__item__link "
-            }
-            to="/contact"
-          >
-            Contact
-          </NavLink>
-        </li>
-      </ul>
+    <nav className="navbar-expand-lg navbar-dark ">
+      <div className="container-fluid ">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className=" collapse navbar-collapse navigation "
+          id="navbarSupportedContent"
+        >
+          <ul className="navigation navbar-nav  me-auto mb-2 mb-lg-0  ">
+            <li className=" nav-item ">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+                exact
+                to="/"
+              >
+                Accueil
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+                to="/realisations"
+              >
+                Réalisations
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+                to="/contact"
+              >
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }
--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,101 +1,164 @@
 // src/pages/Contact/index.jsx
-
-import { Fragment } from "react";
+import React, { Component, Fragment } from "react";
 import Header from "../../components/Header";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 
-function Contact() {
-  return (
-    <Fragment>
-      <Header />
-      <Navbar />
-      <main className="main">
-        <div className="main__container">
-          <div className="main__container__article">
-            <h2 className="main__container__article__title">
-              Contactez-moi <br />{" "}
-              <span
-                style={{
-                  fontSize: "15px",
-                  fontFamily: "arial",
-                }}
-              >
-                Tous les champs sont obligatories{" "}
-              </span>
-            </h2>
-            <form className="contact-form">
-              <label
-                htmlFor="firstName lastName"
-                className="contact-form__label"
-              >
-                Votre identité
-                <div className="contact-form__label__identityContainer">
-                  <input
-                    type="text"
-                    name="firstName"
-                    id="firstName"
-                    className="contact-form__label__input"
-                    placeholder="Prénom"
-                    required
-                  />
-                  <input
-                    type="text"
-                    name="lastName"
-                    id="lastName"
-                    className="contact-form__label__input"
-                    placeholder="Nom"
-                    required
-                  />
-                </div>
-              </label>
-              <label htmlFor="email" className="contact-form__label">
-                Email
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  className="contact-form__label__input"
-                  placeholder="Email"
-                  required
-                />
-              </label>
-              <label htmlFor="topic" className="contact-form__label">
-                Sujet
-                <input
-                  type="text"
-                  name="topic"
-                  id="topic"
-                  className="contact-form__label__input"
-                  placeholder="Sujet"
-                  required
-                />
-              </label>
-              <label htmlFor="message" className="contact-form__label">
-                Votre message
-                <textarea
-                  name="message"
-                  id="message"
-                  cols="20"
-                  rows="10"
-                  className="contact-form__label__input"
-                  required
-                ></textarea>
-              </label>
+const encode = (data) => {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
 
-              <button
-                type="submit"
-                className="contact-form__submit default-btn__opacity__btn"
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      subject: "",
+      message: "",
+      mailSent: false,
+      error: null,
+    };
+  }
+
+  handleSubmit = (e) => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state }),
+    })
+      .then(() => alert("Success!"))
+      .catch((error) => alert(error));
+
+    e.preventDefault();
+  };
+
+  render() {
+    const { firstName, lastName, email, subject, message } = this.state;
+    return (
+      <Fragment>
+        <Header />
+        <Navbar />
+        <main className="main">
+          <div className="main__container">
+            <div className="main__container__article">
+              <h2 className="main__container__article__title">
+                Contactez-moi <br />{" "}
+                <span
+                  style={{
+                    fontSize: "15px",
+                    fontFamily: "arial",
+                  }}
+                >
+                  Tous les champs sont obligatories{" "}
+                </span>
+              </h2>
+              <form
+                className="contact-form"
+                onSubmit={(e) => this.handleContactFormSubmit(e)}
+                method="post"
+                data-netlify="true"
+                name="contact"
               >
-                Envoyer
-              </button>
-            </form>
+                <input type="hidden" name="form-name" value="contact" />
+                <label
+                  htmlFor="firstName lastName"
+                  className="contact-form__label"
+                >
+                  Votre identité
+                  <div className="contact-form__label__identityContainer">
+                    <input
+                      type="text"
+                      name="firstName"
+                      id="firstName"
+                      className="contact-form__label__input"
+                      placeholder="Prénom"
+                      required
+                      value={firstName}
+                      onChange={(e) => {
+                        this.setState({ firstName: e.target.value });
+                      }}
+                    />
+                    <input
+                      type="text"
+                      name="lastName"
+                      id="lastName"
+                      className="contact-form__label__input"
+                      placeholder="Nom"
+                      required
+                      value={lastName}
+                      onChange={(e) => {
+                        this.setState({ lastName: e.target.value });
+                      }}
+                    />
+                  </div>
+                </label>
+                <label htmlFor="email" className="contact-form__label">
+                  Email
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="contact-form__label__input"
+                    placeholder="Email"
+                    required
+                    value={email}
+                    onChange={(e) => {
+                      this.setState({ email: e.target.value });
+                    }}
+                  />
+                </label>
+                <label htmlFor="subject" className="contact-form__label">
+                  Sujet
+                  <input
+                    type="text"
+                    name="subject"
+                    id="subject"
+                    className="contact-form__label__input"
+                    placeholder="Sujet"
+                    required
+                    value={subject}
+                    onChange={(e) => {
+                      this.setState({ subject: e.target.value });
+                    }}
+                  />
+                </label>
+                <label htmlFor="message" className="contact-form__label">
+                  Votre message
+                  <textarea
+                    name="message"
+                    id="message"
+                    cols="20"
+                    rows="10"
+                    className="contact-form__label__input"
+                    required
+                    value={message}
+                    onChange={(e) => {
+                      this.setState({ message: e.target.value });
+                    }}
+                  ></textarea>
+                </label>
+
+                <button
+                  type="submit"
+                  className="contact-form__submit default-btn__opacity__btn"
+                >
+                  Envoyer
+                </button>
+                <div>
+                  {this.state.mailSent && <div>Message bien reçu. Merci.</div>}
+                </div>
+              </form>
+            </div>
           </div>
-        </div>
-      </main>
-      <Footer />
-    </Fragment>
-  );
+        </main>
+        <Footer />
+      </Fragment>
+    );
+  }
 }
 
 export default Contact;
